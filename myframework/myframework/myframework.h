6e2d9bf1b32b893803468715e9e889e8da260e03
kimtaeho <>
2021-02-10 21:53:04 +0900

    4. framework 프로젝트 생성

---
+++ myframework/myframework/myframework.h
@@ -0,0 +1,18 @@
+//
+//  myframework.h
+//  myframework
+//
+//  Created by kimtaeho on 2021/02/10.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for myframework.
+FOUNDATION_EXPORT double myframeworkVersionNumber;
+
+//! Project version string for myframework.
+FOUNDATION_EXPORT const unsigned char myframeworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <myframework/PublicHeader.h>
+
+
