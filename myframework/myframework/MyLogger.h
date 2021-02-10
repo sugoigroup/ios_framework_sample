45f3f30468762e320b9a6f50586bf11fcb961330
kimtaeho <>
2021-02-10 23:34:42 +0900

    8. MyLogger 를 추가

--- myframework/myframework/MyLogger.h
+++ myframework/myframework/MyLogger.h
@@ -11,6 +11,9 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface MyLogger : NSObject
 
++ (instancetype) sharedInstance:(BOOL)argIsEnableLog; //싱글턴 할꺼다.이 프레임웍 사용자가 로그출력여부를 결정하게 할꺼다.
+- (void) printMyLog:(NSString *)argLogText; // 로그 찍을꺼다..
+
 @end
 
 NS_ASSUME_NONNULL_END
--- myframework/myframework/MyLogger.m
+++ myframework/myframework/MyLogger.m
@@ -7,6 +7,33 @@
 
 #import "MyLogger.h"
 
+//isEnableLog은 private 로 선언할꺼다.
+@interface MyLogger()
+@property(nonatomic) BOOL isEnableLog;
+@end
+
 @implementation MyLogger
 
+- (id)initWithEnableLog:(BOOL)argIsEnableLog {
+    self = [super init];
+    if (self) {
+        self.isEnableLog = argIsEnableLog;
+    }
+    return self;
+}
+
+// GCD 싱글톤
++ (id) sharedInstance:(BOOL)argIsEnableLog {
+    static MyLogger *_sharedInstance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _sharedInstance = [[self alloc] initWithEnableLog:argIsEnableLog];
+    });
+    return _sharedInstance;
+}
+
+// 로그 찎자.
+- (void) printMyLog:(NSString *)argLogText {
+    if (_isEnableLog) NSLog(@"Log your name: %@", argLogText);
+}
 @end
