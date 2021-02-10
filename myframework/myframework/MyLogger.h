015d51bfde20328e82403096ac4566ef6a3b9307
kimtaeho <>
2021-02-10 22:17:48 +0900

    7. MyLogger c , h 를 추가하자

---
+++ myframework/myframework/MyLogger.h
@@ -0,0 +1,16 @@
+//
+//  MyLogger.h
+//  myframework
+//
+//  Created by kimtaeho on 2021/02/10.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MyLogger : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ myframework/myframework/MyLogger.m
@@ -0,0 +1,12 @@
+//
+//  MyLogger.m
+//  myframework
+//
+//  Created by kimtaeho on 2021/02/10.
+//
+
+#import "MyLogger.h"
+
+@implementation MyLogger
+
+@end
